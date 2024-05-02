492a76a0f38cc53b1374ba30eadae1b02927cc87
MatthieuCrane <165154156+MatthieuCrane@users.noreply.github.com>
2024-05-02 09:41:02 +0200

    update

--- script.js
+++ script.js
@@ -11,6 +11,88 @@ function getData() {
        console.log('Données récupérées du fichier JSON :', data);
        /// ON ECRIT LE CODE ICI ! 
       
+       let nomjournal = data.journal.nomJournal;
+       console.log(nomjournal);
+       let nomjournalid = document.getElementById("nomjournal");
+       console.log(nomjournalid);
+       nomjournalid.insertAdjacentHTML("beforeend", nomjournal);
+       
+      let Titre = data.journal.articlePrincipal.titre;
+       console.log(Titre);
+
+       let Description = data.journal.articlePrincipal.description;
+       console.log(Description);
+
+       let Date = data.journal.articlePrincipal.date;
+       console.log(Date);
+
+       let Theme = data.journal.articlePrincipal.theme;
+       console.log(Theme); 
+
+       let articlePrincipalid = document.getElementById("articlePrincipal"); 
+       console.log(articlePrincipalid);
+
+       let carte = `<article class="carte">
+       <h2>${Titre}</h2> <p>${Description}</p>
+       <h2>${Date}</h2> <h2>${Theme}</h2>
+       </article>
+        `
+        console.log(carte);
+
+        articlePrincipalid.insertAdjacentHTML("beforeend", carte);
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+       
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+      
+
+
+
+
+
+
+
+
+
 
       
 
